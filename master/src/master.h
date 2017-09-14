0da41913312e7290d3cd6bca48a4b21c25877d33
nicopriano <npriano@outlook.com>
2017-09-14 00:00:26 -0300

    primer commit de master

--- master/src/master.h
+++ master/src/master.h
@@ -12,5 +12,24 @@
 #include <stdlib.h>
 #include <shared-library/connect.h>
 #include <commons/temporal.h>
+#include <commons/log.h>
+#include <commons/config.h>
 
+t_log * logger;
+
+typedef struct {
+	char * ip_yama;
+	char * port_yama;
+} master_cfg;
+master_cfg * master_config;
+
+typedef struct {
+	char * ruta_trans;
+	char * ruta_reduc;
+	char * ruta_orige;
+	char * ruta_resul;
+} pedido_master;
+
+master_cfg * crear_config();
+pedido_master * crear_pedido_yama(char ** argv);
 #endif /* MASTER_H_ */
