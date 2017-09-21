cce485f7578dc588539bc11cffc260a32f7b1f8c
nicopriano <npriano@outlook.com>
2017-09-20 23:32:22 -0300

    atender respuesta yama, generando hilos y mandar a worker

--- master/src/master.c
+++ master/src/master.c
@@ -11,18 +11,22 @@
 #include "master.h"
 #include <commons/log.h>
 #include <commons/config.h>
+#include <shared-library/worker-prot.h>
 
 int main(int argc, char ** argv) {
 
-	if(argc != 5) {
-		printf("ERROR: Cantidad de parametros invalida. Deben ser 4: transformador/ruta \nreductor/ruta \nArchivo de origen/ruta \narchivo resultado/ruta");
+	if (argc != 5) {
+		printf(
+				"ERROR: Cantidad de parametros invalida. Deben ser 4: transformador/ruta \nreductor/ruta \nArchivo de origen/ruta \narchivo resultado/ruta");
 		exit(0);
 	}
+
 	master_config = crear_config();
-
-	int yama_socket = connect_to_socket(master_config->ip_yama, master_config->port_yama);
-
 	pedido_master * pedido = crear_pedido_yama(argv);
+	transformador_file = read_file(pedido->ruta_trans);
+
+	int yama_socket = connect_to_socket(master_config->ip_yama,
+			master_config->port_yama);
 
 	// Enviar Pedido a YAMA
 
@@ -31,26 +35,38 @@ int main(int argc, char ** argv) {
 	respuesta_yama * buffer;
 	int status = recv(yama_socket, operation_code, sizeof(int), 0);
 
-	while(status != -1 && operation_code == 1) {
+	while (status != -1 && operation_code == 1) {
 		status = recv(yama_socket, buffer, sizeof(respuesta_yama), 0);
 
-		if(status != -1) {
+		if (status != -1) {
 
 			// Genero un hilo que atienda la respuesta yama
-			int s; pthread_attr_t attr;
+			int s;
+			pthread_attr_t attr;
 			s = pthread_attr_init(&attr);
-			pthread_t hilo;
+			respuesta_yama * respuesta = malloc(sizeof(respuesta_yama));
 			void* res;
 
-			pthread_create(&hilo, &attr, &atender_respuesta, buffer);
+			s = pthread_create(&respuesta->thread_id, &attr, &atender_respuesta,
+					buffer);
+			if (s != 0) {
 
+			}
 			s = pthread_attr_destroy(&attr);
-			s = pthread_join(hilo, res);
+			if (s != 0) {
+
+			}
+			s = pthread_join(respuesta->thread_id, res);
+			if (s != 0) {
+
+			}
+
 		} else {
 			// atiendo error ?
 		}
 	}
 
+	// REDUCCION LOCAL
 
 
 	char* hora = temporal_get_string_time();
@@ -76,4 +92,64 @@ pedido_master * crear_pedido_yama(char ** argv) {
 	pedido->ruta_resul = argv[4];
 
 	return pedido;
+}
+int atender_respuesta(void * args) {
+	respuesta_yama * respuesta = (respuesta_yama *) args;
+
+	char ** ip_port = string_split(respuesta->ip_port, ":");
+	char * ip = ip_port[0];
+	char * port = ip_port[1];
+	int socket_worker = connect_to_socket(ip, port);
+	transform_req_send(socket_worker, respuesta->bloque,
+			respuesta->bytes_ocupados, respuesta->archivo_temporal,
+			transformador_file->filesize,transformador_file->file, logger);
+
+	t_request_transformation * worker_response = transform_req_recv(socket_worker, logger);
+
+	// NOTIFICA A YAMA
+	if(worker_response.exec_code == SUCCESS) {
+
+	} else if (worker_response->exec_code == ERROR) {
+
+	}
+}
+
+struct_file * read_file(char * path) {
+	FILE * file;
+	void *buffer = malloc(255);
+	struct stat st;
+	char * dir = string_trim(&path);
+	file = fopen(dir, "r");
+
+	if (file) {
+		char * string = string_new();
+
+		fstat(file, &st);
+//		size_t size = st.st_size;
+		struct_file * file_struct = malloc(sizeof(struct_file));
+		file_struct->filesize = st.st_size;
+		file_struct->file = string_new();
+
+		/* POR AHORA LO HAGO POR STRING PORQUE NO SE SACAR EL PAGE OFFSET PARA USAR MMAP
+		 *
+		 fstat(file, &st);
+		 size_t size = st.st_size;
+		 void* script;
+
+		 script = mmap(NULL, size, PROT_READ, MAP_PRIVATE, file,)
+		 */
+
+		while (fgets(buffer, 255, (FILE*) file)) {
+			string_append(&string, buffer);
+
+		}
+		if (feof(file)) {
+			fclose(file);
+			free(buffer);
+			string_append(&(file_struct->file), string);
+			return file_struct;
+		}
+		return NULL;
+	}
+	return NULL;
 }
--- master/src/master.h
+++ master/src/master.h
@@ -13,10 +13,13 @@
 #include <commons/temporal.h>
 #include <commons/log.h>
 #include <commons/config.h>
+#include <commons/collections/list.h>
 #include <pthread.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 t_log * logger;
-
+struct_file * transformador_file;
 typedef struct {
 	char * ip_yama;
 	char * port_yama;
@@ -31,6 +34,7 @@ typedef struct {
 } pedido_master;
 
 typedef struct {
+	pthread_t thread_id;
 	int nodo;
 	char * ip_port;
 	int bloque;
@@ -38,8 +42,21 @@ typedef struct {
 	char * archivo_temporal;
 } respuesta_yama;
 
+typedef struct {
+	char * file;
+	size_t filesize;
+} struct_file;
+
+typedef struct {
+	pthread_t thread_id;
+	int num;
+	char * transformador;
+	int bloque;
+} thread_args_transformacion;
+
 master_cfg * crear_config();
 pedido_master * crear_pedido_yama(char ** argv);
-int atender_respuesta(respuesta_yama * resp);
+int atender_respuesta(void * resp);
+char * read_file(char * path);
 
 #endif /* MASTER_H_ */
